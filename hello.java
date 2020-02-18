947d0ecfce5ee9206dee2e9260cb620765babf5f
root <root@ip-172-31-7-40.us-east-2.compute.internal>
2020-02-18 04:06:30 +0000

    hello

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class hello{
+public static void main(String args[]){
+System.out.println("hello javatpoint");
+System.out.println("hello javatpoint2");
+System.out.println("hello javatpoint3");
+}}
